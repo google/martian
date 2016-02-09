65634fc719a92a03838cb157d853bdbcdcc30a6d
Bramha Ghosh <bramha@google.com>
2016-02-08 17:57:39 -0800

    add skip roundtrip

---
+++ skip/skip.go
@@ -0,0 +1,49 @@
+// Package skip provides a request modifier to skip the HTTP round-trip.
+package skip
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/google/martian/martian"
+	"github.com/google/martian/parse/parse"
+)
+
+// RoundTrip is a modifier that skips the request round-trip.
+type RoundTrip struct{}
+
+type roundTripJSON struct {
+	Scope []parse.ModifierType `json:"scope"`
+}
+
+func init() {
+	parse.Register("skip.RoundTrip", roundTripFromJSON)
+}
+
+// NewRoundTrip returns a new modifier that skips round-trip.
+func NewRoundTrip() *RoundTrip {
+	return &RoundTrip{}
+}
+
+// ModifyRequest skips the request round-trip.
+func (r *RoundTrip) ModifyRequest(req *http.Request) error {
+	ctx := martian.NewContext(req)
+	ctx.SkipRoundTrip()
+
+	return nil
+}
+
+// roundTripFromJSON builds a skip.RoundTrip from JSON.
+
+// Example JSON:
+// {
+//   "skip.RoundTrip": { }
+// }
+func roundTripFromJSON(b []byte) (*parse.Result, error) {
+	msg := &roundTripJSON{}
+	if err := json.Unmarshal(b, msg); err != nil {
+		return nil, err
+	}
+
+	return parse.NewResult(NewRoundTrip(), msg.Scope)
+}
---
+++ skip/skip_test.go
@@ -0,0 +1,51 @@
+package skip
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/google/martian/martian"
+	"github.com/google/martian/parse/parse"
+)
+
+func TestRoundTrip(t *testing.T) {
+	m := NewRoundTrip()
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	if err != nil {
+		t.Fatalf("http.NewRequest(): got %v, want no error", err)
+	}
+
+	ctx, remove, err := martian.TestContext(req)
+	if err != nil {
+		t.Fatalf("martian.TestContext(): got %v, want no error", err)
+	}
+	defer remove()
+
+	if ctx.SkippingRoundTrip() {
+		t.Fatal("ctx.SkippingRoundTrip(): got true, want false")
+	}
+
+	if err := m.ModifyRequest(req); err != nil {
+		t.Fatalf("ModifyRequest(): got %v, want no error", err)
+	}
+
+	if !ctx.SkippingRoundTrip() {
+		t.Fatal("ctx.SkippingRoundTrip(): got false, want true")
+	}
+}
+
+func TestFromJSON(t *testing.T) {
+	msg := []byte(`{
+																																																																								    "skip.RoundTrip": {}
+																																																																									  }`)
+
+	r, err := parse.FromJSON(msg)
+	if err != nil {
+		t.Fatalf("parse.FromJSON(): got %v, want no error", err)
+	}
+
+	reqmod := r.RequestModifier()
+	if _, ok := reqmod.(*RoundTrip); !ok {
+		t.Fatal("reqmod.(*RoundTrip): got !ok, want ok")
+	}
+}
