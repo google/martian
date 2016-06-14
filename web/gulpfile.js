3892ade43ce75beb73ee178943e5307c42c235aa
jmathes <jmathes@google.com>
2016-06-14 15:38:24 -0700

    i don't remember what this is for

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,8 +1,9 @@
 // require('babel/register');
 
-var gulp = require('gulp');
 var babel = require('gulp-babel');
+var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var serve = require('gulp-serve');
 var wct = require('web-component-tester');
 
 wct.gulp.init(gulp);
@@ -17,3 +18,14 @@ gulp.task('test', ['js'], function() {
   return gulp.src('dist/test/**/*-test.js')
     .pipe(mocha());
 });
+
+gulp.task('serve', serve('public'));
+gulp.task('serve-build', serve(['public', 'build']));
+gulp.task('serve-prod', serve({
+  root: ['public', 'build'],
+  port: 443,
+  https: false,
+  // middleware: function(req, res) {
+  //   // custom optional middleware 
+  // }
+}));
