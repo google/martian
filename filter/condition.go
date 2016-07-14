896f5ccd4d82d479497cd85a6a29169c089d0ed1
Bramha Ghosh <bramha@google.com>
2016-07-14 01:30:40 -0600

    getting invert to work

--- filter/condition.go
+++ filter/condition.go
@@ -12,35 +12,44 @@ type RequestCondition interface {
 	MatchRequest(*http.Request) bool
 }
 
-type Condition struct {
-	Request  RequestCondition
-	Response ResponseCondition
-}
-
-func (c *Condition) MatchRequest(req *http.Request) bool {
-	return c.MatchRequest(req)
-}
-
-func (c *Condition) MatchResponse(res *http.Response) bool {
-	return c.MatchResponse(res)
-}
-
-type NotCondition struct {
-	Request  RequestCondition
-	Response ResponseCondition
-}
-
-func (nc *NotCondition) MatchRequest(req *http.Request) bool {
-	return !nc.MatchRequest(req)
-}
-
-func (nc *NotCondition) MatchResponse(res *http.Response) bool {
-	return !nc.MatchResponse(res)
-}
-
-func Invert(c Condition) *NotCondition {
-	return &NotCondition{
-		Request:  c.Request,
-		Response: c.Response,
-	}
-}
+// type Condition struct {
+// 	reqcon RequestCondition
+// 	rescon ResponseCondition
+// }
+//
+// func (c *Condition) RequestCondition() RequestCondition {
+// 	return c.reqcon
+// }
+//
+// func (c *Condition) ResponseCondition() ResponseCondition {
+// 	return c.rescon
+// }
+//
+// func (c *Condition) MatchRequest(req *http.Request) bool {
+// 	return c.MatchRequest(req)
+// }
+//
+// func (c *Condition) MatchResponse(res *http.Response) bool {
+// 	return c.MatchResponse(res)
+// }
+//
+// type NotCondition struct {
+// 	reqcon RequestCondition
+// 	rescon ResponseCondition
+// }
+//
+// func (nc *NotCondition) RequestCondition() RequestCondition {
+// 	return nc.reqcon
+// }
+//
+// func (nc *NotCondition) ResponseCondition() ResponseCondition {
+// 	return nc.rescon
+// }
+//
+// func (nc *NotCondition) MatchRequest(req *http.Request) bool {
+// 	return !nc.MatchRequest(req)
+// }
+//
+// func (nc *NotCondition) MatchResponse(res *http.Response) bool {
+// 	return !nc.MatchResponse(res)
+// }
--- filter/filter_test.go
+++ filter/filter_test.go
@@ -49,3 +49,45 @@ func TestRequestCondition(t *testing.T) {
 		}
 	}
 }
+
+func TestRequestInvertedCondition(t *testing.T) {
+	hm := header.NewMatcher("Martian-Testing", "true")
+	tm := martiantest.NewModifier()
+
+	f := Filter{}
+	f.SetRequestCondition(hm, tm)
+
+	tt := []struct {
+		name   string
+		values []string
+		want   bool
+	}{
+		{
+			name:   "Martian-Production",
+			values: []string{"true"},
+			want:   true,
+		},
+		{
+			name:   "Martian-Testing",
+			values: []string{"see-next-value", "true"},
+			want:   false,
+		},
+	}
+
+	for i, tc := range tt {
+		req, err := http.NewRequest("GET", "/", nil)
+		if err != nil {
+			t.Fatalf("http.NewRequest(): got %v, want no error", err)
+		}
+
+		req.Header[tc.name] = tc.values
+
+		if err := f.ModifyRequest(req); err != nil {
+			t.Fatalf("%d. ModifyRequest(): got %v, want no error", i, err)
+		}
+
+		if tm.RequestModified() != tc.want {
+			t.Errorf("%d. tm.RequestModified(): got %t, want %t", i, tm.RequestModified(), tc.want)
+		}
+	}
+}
