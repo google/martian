ab2eb5c3b97c68bc237040708f5ac93e979dc0b5
Joe <jmathes@gmail.com>
2016-08-16 16:55:10 -0700

    marbl logs track latency with a :latency header (#106)
    * add timestamps to marbl logging
    
    * add package and func comments

--- marbl/handler.go
+++ marbl/handler.go
@@ -1,3 +1,17 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package marbl
 
 import (
@@ -11,17 +25,21 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+// Handler exposes marbl logs over websockets.
 type Handler struct {
 	mu   sync.RWMutex
 	subs map[string]chan<- []byte
 }
 
+// NewHandler instantiates a Handler with an empty set of subscriptions.
 func NewHandler() *Handler {
 	return &Handler{
 		subs: make(map[string]chan<- []byte),
 	}
 }
 
+// Write writes frames to all websocket subscribers and returns the number
+// of bytes written and an error.
 func (h *Handler) Write(b []byte) (int, error) {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
