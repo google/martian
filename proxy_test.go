b455c343b2d0b9c31123f9e1253c716b3ca9982e
Bramha Ghosh <bramha@google.com>
2016-08-22 12:30:09 -0700

    Fix chunked encoding bug: reset connection on response_writer Close() (#110)
    * reset the writer on closed if chunked

--- proxy_test.go
+++ proxy_test.go
@@ -504,7 +504,7 @@ func TestIntegrationTLSHandshakeErrorCallback(t *testing.T) {
 		t.Fatalf("mitm.NewConfig(): got %v, want no error", err)
 	}
 
-	// TODO: herr is not never being asserted against. It should be pushed on to a channel
+	// TODO: herr is not being asserted against. It should be pushed on to a channel
 	// of err, and the assertion should pull off of it and assert. That design resulted in the test
 	// hanging for unknown reasons.
 	var herr error
