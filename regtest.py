f5831baa358c757207cfc0f854cf2cd70b58cee2
Huichao Xue <xuehuichao@google.com>
2017-03-27 17:21:46 -0700

    Added testcase for serialization

--- regtest.py
+++ regtest.py
@@ -3,6 +3,7 @@ import unittest
 import time
 import json
 import BaseHTTPServer
+import tempfile
 import logging
 import urllib2
 import sys
@@ -46,18 +47,21 @@ class CountingServer(BaseHTTPServer.HTTPServer):
     BaseHTTPServer.HTTPServer.__init__(self, *args, **kw)
     self.count = 0
 
+class MartianAndBackendServer(object):
 
-class TestCacheAndReplay(unittest.TestCase):
-
-  def setUp(self):
-    self._martian_port = portpicker.pick_unused_port()
-    self._martian_api_port = portpicker.pick_unused_port()
-    self._backend_port = portpicker.pick_unused_port()
+  def __init__(self,
+               martian_port,
+               martian_api_port,
+               backend_port,
+               additional_flags=[]):
+    self._martian_port = martian_port
+    self._martian_api_port = martian_api_port
+    self._backend_port = backend_port
 
     self._martian_proc = subprocess.Popen([
         os.path.join(os.getcwd(), "main"), "-addr", ":%d" % self._martian_port,
         "-api-addr", ":%d" % self._martian_api_port, "-v", "2"
-    ])
+    ] + additional_flags)
     self._http_server = CountingServer(("", self._backend_port),
                                        CountingServiceHandler)
     logging.info("Started martian on port %d, API port %d", self._martian_port,
@@ -70,7 +74,7 @@ class TestCacheAndReplay(unittest.TestCase):
     logging.info("Fetching http server until it's ready")
     while True:
       try:
-        current_count = self._GetCurrentNumberDirectly()
+        current_count = self.GetCurrentNumberDirectly()
         logging.info("Current http server count is %s", current_count)
         break
       except IOError:
@@ -78,21 +82,21 @@ class TestCacheAndReplay(unittest.TestCase):
         time.sleep(1)
     logging.info("Http server ready.")
 
-  def tearDown(self):
+  def ShutDown(self):
     self._http_server.shutdown()
     if self._martian_proc.poll() is None:
       self._martian_proc.terminate()
       while self._martian_proc.poll() is None:
         time.sleep(1)
 
-  def _GetCurrentNumberDirectly(self):
+  def GetCurrentNumberDirectly(self):
     return int(
         urllib2.urlopen("http://localhost:%d" % self._backend_port).read())
 
-  def _GetNumberThroughProxy(self):
-    return int(self._GetPathThroughProxy())
+  def GetNumberThroughProxy(self):
+    return int(self.GetPathThroughProxy())
 
-  def _GetPathThroughProxy(self, path=""):
+  def GetPathThroughProxy(self, path=""):
     proxy = urllib2.ProxyHandler({
         "http": "http://localhost:%d" % self._martian_port
     })
@@ -101,7 +105,7 @@ class TestCacheAndReplay(unittest.TestCase):
                                                      path)).read()
     return content
 
-  def _PostJsonConfigToMartian(self, config_dict):
+  def PostJsonConfigToMartian(self, config_dict):
     req = urllib2.Request("http://martian.proxy/configure")
     req.add_header("Content-Type", "application/json")
     proxy = urllib2.ProxyHandler({
@@ -112,26 +116,81 @@ class TestCacheAndReplay(unittest.TestCase):
     logging.info("Posted config %r to martian, and got %r", config_dict,
                  response)
 
-  def testRecordAndReplayUsecase(self):
-    self.assertEquals(1, self._GetCurrentNumberDirectly())
-    self.assertEquals(2, self._GetNumberThroughProxy())
 
-    self._PostJsonConfigToMartian({"cache.Modifier": {"mode": "cache"}})
-    self.assertEquals(3, self._GetNumberThroughProxy())
-    self._PostJsonConfigToMartian({"cache.Modifier": {"mode": "replay"}})
-    self.assertEquals(3, self._GetNumberThroughProxy())
+class TestCacheAndReplay(unittest.TestCase):
+
+  def setUp(self):
+    self._martian_port = portpicker.pick_unused_port()
+    self._martian_api_port = portpicker.pick_unused_port()
+    self._backend_port = portpicker.pick_unused_port()
+
+    self._server_setup = MartianAndBackendServer(
+        self._martian_port, self._martian_api_port, self._backend_port)
+
+  def tearDown(self):
+    self._server_setup.ShutDown()
+
+  def testRecordAndReplayUsecase(self):
+    self.assertEquals(1, self._server_setup.GetCurrentNumberDirectly())
+    self.assertEquals(2, self._server_setup.GetNumberThroughProxy())
+
+    self._server_setup.PostJsonConfigToMartian({
+        "cache.Modifier": {
+            "mode": "cache"
+        }
+    })
+    self.assertEquals(3, self._server_setup.GetNumberThroughProxy())
+    self._server_setup.PostJsonConfigToMartian({
+        "cache.Modifier": {
+            "mode": "replay"
+        }
+    })
+    self.assertEquals(3, self._server_setup.GetNumberThroughProxy())
     # The request does not go to the backend
-    self.assertEquals(4, self._GetCurrentNumberDirectly())
+    self.assertEquals(4, self._server_setup.GetCurrentNumberDirectly())
 
   def testReplayNotCachedURL(self):
-    self.assertEquals(1, self._GetCurrentNumberDirectly())
-    self._PostJsonConfigToMartian({"cache.Modifier": {"mode": "cache"}})
-    self.assertEquals(2, self._GetNumberThroughProxy())
+    self.assertEquals(1, self._server_setup.GetCurrentNumberDirectly())
+    self._server_setup.PostJsonConfigToMartian({
+        "cache.Modifier": {
+            "mode": "cache"
+        }
+    })
+    self.assertEquals(2, self._server_setup.GetNumberThroughProxy())
 
-    self._PostJsonConfigToMartian({"cache.Modifier": {"mode": "replay"}})
-    self.assertEquals(2, self._GetNumberThroughProxy())
-    self.assertEquals("/not_cached", self._GetPathThroughProxy("/not_cached"))
-    self.assertEquals(3, self._GetCurrentNumberDirectly())
+    self._server_setup.PostJsonConfigToMartian({
+        "cache.Modifier": {
+            "mode": "replay"
+        }
+    })
+    self.assertEquals(2, self._server_setup.GetNumberThroughProxy())
+    self.assertEquals("/not_cached",
+                      self._server_setup.GetPathThroughProxy("/not_cached"))
+    self.assertEquals(3, self._server_setup.GetCurrentNumberDirectly())
+
+
+class TestSerialization(unittest.TestCase):
+
+  def testSerAndDeser(self):
+    martian_port = portpicker.pick_unused_port()
+    martian_api_port = portpicker.pick_unused_port()
+    backend_port = portpicker.pick_unused_port()
+
+    cache_file = tempfile.NamedTemporaryFile()
+    cache_file_path = cache_file.name
+
+    server = MartianAndBackendServer(martian_port, martian_api_port,
+                                     backend_port, ["-cache", cache_file_path])
+    self.assertEquals(1, self._server_setup.GetCurrentNumberDirectly())
+    self.assertEquals(2, self._server_setup.GetCurrentNumberDirectly())
+    server.PostJsonConfigToMartian({"cache.Modifier": {"mode": "cache"}})
+    self.assertEquals(3, server.GetNumberThroughProxy())
+    server.ShutDown()
+
+    server = MartianAndBackendServer(martian_port, martian_api_port,
+                                     backend_port, ["-cache", cache_file_path])
+    server.PostJsonConfigToMartian({"cache.Modifier": {"mode": "replay"}})
+    self.assertEquals(3, server.GetNumberThroughProxy())
 
 
 if __name__ == "__main__":
