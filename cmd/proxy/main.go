5129aa8af80380931ca308ec572e164e5e51ac79
Addison Luh <hueich@gmail.com>
2017-10-10 21:59:17 -0700

    Add cache modifier to proxy/main

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -220,6 +220,7 @@ import (
 	"github.com/google/martian/verify"
 
 	_ "github.com/google/martian/body"
+	_ "github.com/google/martian/cache"
 	_ "github.com/google/martian/cookie"
 	_ "github.com/google/martian/failure"
 	_ "github.com/google/martian/martianurl"
