080d272807cc8281b0f3fda922f8f628d4f52c0f
Bramha Ghosh <bramha@google.com>
2018-05-31 14:14:10 -0700

    Update travis.yml to do linting and fix a linter error (#252)
    * update travis.yml to do linting and tests with race detection
    
    * use golint
    
    * add websocket install
    
    * add godoc for IgnoreSIGPIPE
    
    * not doing race detection because we have detected a race

--- nosigpipe/nosigpipe.go
+++ nosigpipe/nosigpipe.go
@@ -4,5 +4,7 @@ package nosigpipe
 
 import "net"
 
+// IgnoreSIGPIPE prevents SIGPIPE from being raised on TCP sockets when remote hangs up
+// See: https://github.com/golang/go/issues/17393. Do nothing for non Darwin
 func IgnoreSIGPIPE(c net.Conn) {
 }
