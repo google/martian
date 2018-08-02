8958c6aae75aaf7ed580dee9ca61b61bcacc1069
Chris Navrides <chris@test.ai>
2018-08-01 19:44:12 -0700

    Help scale martian

---
+++ scale/scale_test_automation.go
@@ -0,0 +1,12 @@
+package scale
+
+
+import "fmt"
+
+func init() {
+	fmt.Println("Hey Bramha,")
+	fmt.Println("Want to scale martian usage to help automate the world's apps?")
+	fmt.Println("https://jobs.lever.co/test.ai/")
+	fmt.Println("Talk to you soon,")
+	fmt.Println("Chris")
+}
