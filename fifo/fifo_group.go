343b078ab53c9b993698164bc615acc716025bab
Michał Matczuk <michal.matczuk@saucelabs.com>
2022-11-22 14:21:55 +0100

    fifo: remove logging
    Those logs litter the debug logs and add little value, see example below
    
    DEBUG: martian: waiting for request: 172.26.0.5:48240
    DEBUG: fifo.ModifyRequest: http://httpbin/status/202
    DEBUG: fifo.ModifyRequest: http://httpbin/status/202
    DEBUG: fifo.ModifyRequest: http://httpbin/status/202
    DEBUG: fifo.ModifyResponse: http://httpbin/status/202
    DEBUG: fifo.ModifyResponse: http://httpbin/status/202
    DEBUG: fifo.ModifyResponse: http://httpbin/status/202
    DEBUG: fifo.ModifyResponse: http://httpbin/status/202

--- fifo/fifo_group.go
+++ fifo/fifo_group.go
@@ -26,7 +26,6 @@ import (
 	"sync"
 
 	"github.com/google/martian/v3"
-	"github.com/google/martian/v3/log"
 	"github.com/google/martian/v3/parse"
 	"github.com/google/martian/v3/verify"
 )
@@ -89,7 +88,6 @@ func (g *Group) AddResponseModifier(resmod martian.ResponseModifier) {
 // aggregateErrors is set to true, the errors returned by each modifier in the group are
 // aggregated.
 func (g *Group) ModifyRequest(req *http.Request) error {
-	log.Debugf("fifo.ModifyRequest: %s", req.URL)
 	g.reqmu.RLock()
 	defer g.reqmu.RUnlock()
 
@@ -118,11 +116,6 @@ func (g *Group) ModifyRequest(req *http.Request) error {
 // aggregateErrors is set to true, the errors returned by each modifier in the group are
 // aggregated.
 func (g *Group) ModifyResponse(res *http.Response) error {
-	requ := ""
-	if res.Request != nil {
-		requ = res.Request.URL.String()
-		log.Debugf("fifo.ModifyResponse: %s", requ)
-	}
 	g.resmu.RLock()
 	defer g.resmu.RUnlock()
 
@@ -149,7 +142,6 @@ func (g *Group) ModifyResponse(res *http.Response) error {
 // VerifyRequests returns a MultiError containing all the
 // verification errors returned by request verifiers.
 func (g *Group) VerifyRequests() error {
-	log.Debugf("fifo.VerifyRequests()")
 	g.reqmu.Lock()
 	defer g.reqmu.Unlock()
 
@@ -175,7 +167,6 @@ func (g *Group) VerifyRequests() error {
 // VerifyResponses returns a MultiError containing all the
 // verification errors returned by response verifiers.
 func (g *Group) VerifyResponses() error {
-	log.Debugf("fifo.VerifyResponses()")
 	g.resmu.Lock()
 	defer g.resmu.Unlock()
 
@@ -200,7 +191,6 @@ func (g *Group) VerifyResponses() error {
 
 // ResetRequestVerifications resets the state of the contained request verifiers.
 func (g *Group) ResetRequestVerifications() {
-	log.Debugf("fifo.ResetRequestVerifications()")
 	g.reqmu.Lock()
 	defer g.reqmu.Unlock()
 
@@ -213,7 +203,6 @@ func (g *Group) ResetRequestVerifications() {
 
 // ResetResponseVerifications resets the state of the contained request verifiers.
 func (g *Group) ResetResponseVerifications() {
-	log.Debugf("fifo.ResetResponseVerifications()")
 	g.resmu.Lock()
 	defer g.resmu.Unlock()
 
@@ -227,15 +216,16 @@ func (g *Group) ResetResponseVerifications() {
 // groupFromJSON builds a fifo.Group from JSON.
 //
 // Example JSON:
-// {
-//   "fifo.Group" : {
-//     "scope": ["request", "result"],
-//     "modifiers": [
-//       { ... },
-//       { ... },
-//     ]
-//   }
-// }
+//
+//	{
+//	  "fifo.Group" : {
+//	    "scope": ["request", "result"],
+//	    "modifiers": [
+//	      { ... },
+//	      { ... },
+//	    ]
+//	  }
+//	}
 func groupFromJSON(b []byte) (*parse.Result, error) {
 	msg := &groupJSON{}
 	if err := json.Unmarshal(b, msg); err != nil {
